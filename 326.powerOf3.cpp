ae7d1f2cff13ab8a973f008bc971276102477eb2
psyreddy <108641904+psyreddy@users.noreply.github.com>
2023-06-03 15:39:10 +0530

    Easy

---
+++ 326.powerOf3.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<=0)
+            return false;
+        for(int i=0;i<20;i++)
+        if(pow(3,i)==n)
+        return true;
+        return false;
+    }
+};
