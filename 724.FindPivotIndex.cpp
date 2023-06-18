9520e7330a1da1769c5a3fdd833871f42070d90b
psyreddy <108641904+psyreddy@users.noreply.github.com>
2023-06-18 21:32:10 +0530

    Easy

---
+++ 724.FindPivotIndex.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum = 0,lsum=0,rsum=0;
+        for(int i=0;i<nums.size();i++)
+        sum+=nums[i];
+        if(sum - nums[0] == 0)
+        return 0;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            lsum += nums[i];
+            rsum = sum - nums[i+1] - lsum;
+            if(lsum == rsum)
+            return i+1;
+        }
+        return -1;
+    }
+};
