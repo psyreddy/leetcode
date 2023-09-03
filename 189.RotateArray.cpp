239aeeaff297dc44823593e3d7f91c1152beb08d
psyreddy <108641904+psyreddy@users.noreply.github.com>
2023-09-03 21:07:07 +0530

    Medium

---
+++ 189.RotateArray.cpp
@@ -0,0 +1,18 @@
+// Brute force approach
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k%n;
+        while(k){
+            int temp = nums[n-1];
+            for(int i=n-1;i>0;i--){
+                nums[i] = nums[i-1];
+            }
+            nums[0] = temp;
+            k--;
+        }
+    }
+};
+
