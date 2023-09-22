b7346fc008692b4c87a000c3d08d646c3a16d697
psyreddy <108641904+psyreddy@users.noreply.github.com>
2023-09-22 20:22:11 +0530

    Easy

---
+++ 88.MergeSortedArray.cpp
@@ -0,0 +1,37 @@
+// Brute force
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> ans;
+        int i =0 ,j=0; 
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+                ans.push_back(nums1[i]);
+                i++;
+            }
+            else if(nums1[i]>nums2[j]){
+                ans.push_back(nums2[j]);
+                j++;
+            }
+            else if(nums1[i]==nums2[j]){
+                ans.push_back(nums1[i]);
+                i++;
+                ans.push_back(nums2[j]);
+                j++;
+            }
+        }
+        while(j<n){
+            ans.push_back(nums2[j]);
+            j++;
+        }
+        while(i<m){
+            ans.push_back(nums1[i]);
+            i++;
+        }
+        nums1 = ans;
+    }
+};
+
+
+// Optimal without using extra space
+
