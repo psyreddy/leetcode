b344f2245545cd6f71ece5238cac2d6877daac63
psyreddy <108641904+psyreddy@users.noreply.github.com>
2023-09-27 17:24:21 +0530

    Easy

--- 110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree.cpp
@@ -31,13 +31,18 @@
 // Optimised version
 
 class Solution {
-private:
-    int height(TreeNode* root){
-        
-    }
 public:
+    int height(TreeNode* root){
+        if(root==NULL) return 0;
+        int l = height(root->left);
+        int r = height(root->right);
+        if(l==-1 || r==-1) return -1;
+        if(abs(l-r)>1) return -1;
+        return 1 + max(l,r);
+    }
     bool isBalanced(TreeNode* root) {
-        if(root==NULL) return true;
         
+        if(height(root) != -1) return true;
+        return false;
     }
 };
