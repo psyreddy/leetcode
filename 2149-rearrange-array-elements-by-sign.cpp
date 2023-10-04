bf2f853a437804ce63a70b3b5f3771a818967845
psyreddy <108641904+psyreddy@users.noreply.github.com>
2023-10-04 12:30:11 +0530

    Medium

--- 2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign.cpp
@@ -1,4 +1,4 @@
-// Brute force using extra space 
+// Brute force 2 passes through array
 
 class Solution {
 public:
@@ -26,3 +26,25 @@ public:
     }
 };
 
+
+// only one pass through array
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> ans(nums.size(),0);
+        int po = 0;
+        int ne = 1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>0){
+                ans[po] = nums[i];
+                po+=2;
+            }
+            else{
+                ans[ne] = nums[i];
+                ne+=2;
+            }
+        }
+        return ans;
+    }
+};
