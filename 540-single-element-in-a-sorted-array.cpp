8d32f06cdec92ddea6e0609356dc25d7487a4da0
psyreddy <108641904+psyreddy@users.noreply.github.com>
2023-10-04 12:31:05 +0530

    Medium

---
+++ 540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,13 @@
+// Brute force O(n) using xor
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int ans=0 ;
+        for(int i=0;i<nums.size();i++){
+            ans = ans^nums[i];
+        }
+        return ans;
+    }
+};
+
