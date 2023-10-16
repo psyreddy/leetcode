de909588519796479bc5d56a3c1753ad3aea4b2b
psyreddy <108641904+psyreddy@users.noreply.github.com>
2023-10-16 15:31:51 +0530

    Easy

--- 234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list.cpp
@@ -9,8 +9,35 @@
  * };
  */
 class Solution {
+private:
+    ListNode* rev(ListNode* head){
+        if(head==NULL){
+            return head;
+        }
+        if(head->next==NULL){
+            return head;
+        }
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* nxt;
+        // while(curr->next!=NULL){
+        //     nxt = curr->next;
+        //     curr->next = prev;
+        //     prev = curr;
+        //     curr = nxt;
+        // }
+        while(curr!=NULL){
+            nxt = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+        return prev;
+    }
 public:
     bool isPalindrome(ListNode* head) {
+        // brute force
+        /*  
         if(head->next==NULL) return true;
         queue<int> q;
         stack<ListNode*> s;
@@ -30,7 +57,23 @@ public:
             }
         }
         return true;
+        */
 
+        // Optimal 
+        ListNode* s = head;
+        ListNode* f = head;
+        ListNode* begin = head;
+        while(f->next!=NULL && f->next->next!=NULL){
+            s=s->next;
+            f=f->next->next;
+        }
+        s->next = rev(s->next);
+        s = s->next;
+        while(s!=NULL){
+            if(begin->val!=s->val) return 0;
+            begin = begin->next;
+            s = s->next;
+        }
+        return 1;
     }
 };
-
