fb9fd9e7b44fe9886925148fdbe81342e446437f
psyreddy <108641904+psyreddy@users.noreply.github.com>
2023-11-29 08:26:40 +0530

    Medium

--- 75.SortColors.cpp
+++ 75.SortColors.cpp
@@ -16,3 +16,27 @@ public:
         }
     }
 };
+
+
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& a) {
+        int mid=0,low=0,high=a.size()-1;
+        while(mid<=high){
+            if(a[mid]==0){
+                swap(a[mid],a[low]);
+                low++;
+                mid++;
+            }
+            else if(a[mid]==1){
+                mid ++;
+            }
+            else{
+                swap(a[mid],a[high]);
+                high--;
+            }
+        }
+    }
+};
